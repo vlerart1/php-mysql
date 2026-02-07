10a98858a903eb1e98934c0525b2389ebd3577ad
vlerart1 <167799326+vlerart1@users.noreply.github.com>
2026-02-07 14:12:33 +0100

    Update vs.php

--- moduli3/vs.php
+++ moduli3/vs.php
@@ -1,69 +1,105 @@
 <?php
-$var1 = 10;
-if ($var1 >0 ) {
-    echo"$var1 is bigger than 0 <hr>";
-}
+// $var1 = 10;
+// if ($var1 >0 ) {
+//     echo"$var1 is bigger than 0 <hr>";
+// }
 
-$age = 19;
-if ($age >18 ) {
-    echo" you can vote<hr>";
-}
-else  {
-    echo"you can not vote";
-}
-
-
-$a = 5;
-$b = 10;
-
-if ($a > $b) {
-    echo "a is bigger than b <hr>";
-} else if ($a < $b) {
-    echo "b is bigger than a <hr>";
-} else {
-    echo "a and b are equal <hr>";
+// $age = 19;
+// if ($age >18 ) {
+//     echo" you can vote<hr>";
+// }
+// else  {
+//     echo"you can not vote";
+// }
 
 
+// $a = 5;
+// $b = 10;
 
-}
+// if ($a > $b) {
+//     echo "a is bigger than b <hr>";
+// } else if ($a < $b) {
+//     echo "b is bigger than a <hr>";
+// } else {
+//     echo "a and b are equal <hr>";
 
-switch ($age) {
-    case ($age >= 0 && $age < 18):
-        echo "You are a minor.";
-        break;
 
-    case ($age >= 18 && $age < 25):
-        echo "You are a young adult";
-        break;
 
-    case ($age >= 25 && $age < 65):
-        echo "You are middle age.";
-        break;
+// }
 
-    case ($age >= 65):
-        echo "You are a senior.";
-        break;
+// switch ($age) {
+//     case ($age >= 0 && $age < 18):
+//         echo "You are a minor.";
+//         break;
 
-    default:
-        echo "Invalid age value.";
-    }
-    echo "<hr>";
+//     case ($age >= 18 && $age < 25):
+//         echo "You are a young adult";
+//         break;
+
+//     case ($age >= 25 && $age < 65):
+//         echo "You are middle age.";
+//         break;
+
+//     case ($age >= 65):
+//         echo "You are a senior.";
+//         break;
+
+//     default:
+//         echo "Invalid age value.";
+//     }
+//     echo "<hr>";
     
 
-$x = 10;
+// $x = 10;
 
-while ($x <= 15) {
-    echo $x . "\n";
-    $x++;
+// while ($x <= 15) {
+//     echo $x . "\n";
+//     $x++;
+// }
+// $z=10;
+// do {
+//     echo "z is now: $z <hr>";
+//     $z++;
+// }while ($z <= 100);
+
+
+
+
+
+$sports = array("Football", "Basketball", "Voleyball", "a", "s", "c");
+
+$sports2 = ["Football", "Basketball", "Voleyball"];
+
+echo $sports[2] . "<hr>";
+
+echo end($sports) . "<hr>";
+
+echo count($sports) . "<hr>";
+
+for ($count = 0; $count < 6; $count++) {
+    echo $sports[$count] . "<hr>";
 }
-$z=10;
-do {
-    echo "z is now: $z <hr>";
-    $z++;
-}while ($z <= 100);
 
+array_push($sports, "Golf");
+var_dump($sports);
+
+array_pop($sports);
+var_dump($sports);
+
+array_unshift($sports, "Tennis"); // ✅ fixed here
+var_dump($sports);
+
+array_shift($sports);
+var_dump($sports);
+
+$numbers = [1,2,3,4,5,6,7,8,9,10];
+$mbledhja = array_sum($numbers);
+
+echo $mbledhja . "<hr>";
 
 ?>
+
+
 
 
 
